6ad8e58c9db9b13720cc933c36542b5307c70c86
kuanglinfei <helloklf@outlook.com>
2018-10-26 17:43:06 +0800

    更新

--- kr-script/src/main/assets/custom/actions/low_power_level_set.sh
+++ kr-script/src/main/assets/custom/actions/low_power_level_set.sh
@@ -1,4 +1,17 @@
 #!/system/bin/sh
 
-settings put global low_power_trigger_level $level
-settings put global low_power_trigger_level_max $levelmax
+if [[ "$level" = "null" ]]
+then
+    settings delete global low_power_trigger_level
+    settings reset global low_power_trigger_level
+else
+    settings put global low_power_trigger_level $level
+fi
+
+if [[ "$levelmax" = "null" ]]
+then
+    settings delete global low_power_trigger_level_max
+    settings reset global low_power_trigger_level_max
+else
+    settings put global low_power_trigger_level_max $levelmax
+fi
