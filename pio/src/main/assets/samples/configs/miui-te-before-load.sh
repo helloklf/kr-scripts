b5f244dfaa3f14745f8ee34b81e4040e2f30c06c
kuanglinfei <helloklf@outlook.com>
2019-12-02 18:29:57 +0800

    优化部分按钮点击体验

---
+++ pio/src/main/assets/samples/configs/miui-te-before-load.sh
@@ -0,0 +1,20 @@
+# 解密MIUI的温控文件
+file_name="miui-thermal"
+remote_url="https://vtools.oss-cn-beijing.aliyuncs.com/pio/$file_name"
+download_dir="$START_DIR/miui-thermal-engine"
+programma="$download_dir/$file_name"
+output_dir="/sdcard/backups/miui-thermal/outputs"
+
+if [[ ! -f "$programma" ]];
+then
+    mkdir -p "$download_dir"
+    curl -o "$programma" $remote_url
+fi
+
+chmod -R 755 $download_dir
+chown -R $APP_USER_ID:$APP_USER_ID $download_dir
+
+if [[ -f "$programma" ]] && [[ ! -d "$output_dir" ]]; then
+    mkdir -p "$output_dir"
+    $programma -o="$output_dir" -d="true" -i="/vendor/etc"
+fi
